596dacbdced1d58839825ed90be0188ccca89737
Cody Lee <notlimaheelydoc@gmail.com>
2024-04-14 00:00:04 -0500

    additem

--- src/Components/sellerComponents/itemPhotoUpload.js
+++ src/Components/sellerComponents/itemPhotoUpload.js
@@ -9,21 +9,34 @@ import {
 } from "../ui/photoUploadCarousel";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
-function ItemPhotoUpload() {
-  const [fileUpload, setFileUpload] = useState();
+function ItemPhotoUpload({ setFileUpload, fileUpload }) {
   return (
     <div className="flex flex-col bg-black h-full ">
       <h1 className="mx-auto text-darkgold mt-5 mb-5 text-xl">Upload Photos</h1>
-      <Carousel className=" w-10/12   mx-auto ">
+      <Carousel className=" w-10/12  mx-auto ">
         <CarouselContent>
-          {Array.from({ length: 5 }).map((_, index) => (
+          {fileUpload.map((photo, index) => (
             <CarouselItem key={index}>
-              <div className="p-1">
-                <Card>
-                  <CardContent className="flex aspect-square items-center justify-center p-6">
-                    <span className="text-4xl font-semibold">{index + 1}</span>
+              <div className="p-1 ">
+                <Card className="border-solid border-2 border-darkgold rounded-lg ">
+                  <CardContent className="flex aspect-square flex-col items-center justify-center p-6">
+                    <img
+                      key={index}
+                      className="border-solid border-2 border-darkgold rounded-lg "
+                      src={URL.createObjectURL(photo)}
+                    />
+                    <Button
+                      onClick={() => {
+                        const deletedImg = [...fileUpload];
+                        deletedImg.splice(index, 1);
+                        setFileUpload(deletedImg);
+                        console.log(fileUpload);
+                      }}
+                      className="w-11/12 border-2 border-solid border-darkgold mt-2 h-8"
+                    >
+                      Delete
+                    </Button>
                   </CardContent>
-                  <Button className="w-full h-5">Delete</Button>
                 </Card>
               </div>
             </CarouselItem>
@@ -38,7 +51,12 @@ function ItemPhotoUpload() {
         content="upload"
         placeholder="Upload Photos"
         className="  mx-auto w-10/12 mt-5  "
-        onChange={(e) => setFileUpload(e.target.files)}
+        onChange={(e) => {
+          const files = Array.from(e.target.files || []);
+
+          setFileUpload(files);
+        }}
+        multiple
       />
     </div>
   );
--- src/Pages/SellerPages/AddItem.js
+++ src/Pages/SellerPages/AddItem.js
@@ -1,4 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../../Components/ui/form";
+
 import {
   Select,
   SelectContent,
@@ -10,65 +24,240 @@ import {
 } from "../../Components/ui/select";
 import { Input } from "../../Components/ui/input";
 import { Button } from "../../Components/ui/button";
+import { Textarea } from "../../Components/ui/textarea";
 import ItemPhotoUpload from "../../Components/sellerComponents/itemPhotoUpload";
-function addItem() {
-  return (
-    <div className=" grid sm:grid-cols-2 xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4  border-black border-4 border-solid ">
-      <div className="w-full xs:w-1/2 col-span-1 h-screen">
-        <ItemPhotoUpload />
-      </div>
-      <div className="grid grid-cols-1 col-span-1 lg:col-span-2 lg:grid-cols-2 ">
-        <div className=" col-span-1 ">
-          <Input placeholder="Enter Item Name..." />
-          <Input placeholder="Enter Item Description..." />
-          <Input placeholder="Serial Number" />
-          <Input placeholder="Enter Price" />
-        </div>
-        <div className="col-span-1 ">
-          <Select>
-            <SelectTrigger className="w-1/6">
-              <SelectValue placeholder="Category" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectItem value="active">Gems</SelectItem>
-                <SelectItem value="inactive">Jewelry</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-          <Input placeholder="separate with commas" />
-          <Select>
-            <SelectTrigger className="w-1/6">
-              <SelectValue placeholder="Select Status" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectItem value="active">Active</SelectItem>
-                <SelectItem value="inactive">Inactive</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
 
-          <Select>
-            <SelectTrigger className="w-1/6">
-              <SelectValue placeholder="Select Status" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectItem value="active">Active</SelectItem>
-                <SelectItem value="inactive">Inactive</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-          <Input placeholder="Enter Quantity For Out Of Stock..." />
-          <Input placeholder="Warranty Information" />
-          <Input placeholder="Item Information" />
-          <Input placeholder="Weight" />
-          <Button>Add Item</Button>
-        </div>
+function AddItem() {
+  const [fileUpload, setFileUpload] = useState([]);
+  const [missing, setMissing] = useState();
+  const { control } = useForm();
+  const formSchema = z.object({
+    username: z.string().min(2, {
+      message: "Name Required",
+    }),
+  });
+  const [formData, setFormData] = useState({
+    name: "",
+    serial: "",
+    price: "",
+    description: "",
+    category: "",
+    tags: "",
+    sale: "",
+    inStock: "",
+    stock: "",
+    warranty: "",
+    extra: "",
+    weight: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const submitItem = () => {
+    const emptyFields = Object.entries(formData)
+      .filter(([key, value]) => value === "")
+      .map(([key]) => key)
+      .toString();
+    emptyFields.length > 0
+      ? setMissing("Missing Fields: All Fields Required")
+      : setMissing();
+  };
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(values) {
+    // Do something with the form values.
+    console.log(values);
+  }
+
+  return (
+    <div className=" grid  sm:grid-cols-2 xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-3   border-black border-4 border-solid ">
+      <div className="w-full  xs:w-1/2 col-span-1 h-screen">
+        <ItemPhotoUpload
+          setFileUpload={setFileUpload}
+          fileUpload={fileUpload}
+        />
+      </div>
+
+      {/* <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form> */}
+
+      <div className="grid    mt-5 grid-cols-1 col-span-1 lg:col-span-2 lg:grid-cols-2 ">
+        <Input
+          onChange={(e) => {
+            handleInputChange(e);
+          }}
+          className="w-10/12 mx-auto"
+          placeholder="Enter Item Name..."
+          type="text"
+          name="name"
+          value={formData.name}
+        />
+
+        <Input
+          onChange={(e) => {
+            handleInputChange(e);
+          }}
+          className="w-10/12 mx-auto"
+          placeholder="Serial Number"
+          name="serial"
+          value={formData.serial}
+        />
+        <Input
+          onChange={(e) => {
+            handleInputChange(e);
+          }}
+          className="w-10/12 mx-auto"
+          placeholder="Price"
+          name="price"
+          value={formData.price}
+        />
+        <Textarea
+          onChange={(e) => {
+            handleInputChange(e);
+          }}
+          className="resize-none w-10/12 mx-auto h-1/2"
+          placeholder="Item Description..."
+          name="description"
+          value={formData.description}
+        />
+        <Select
+          onValueChange={(e) => setFormData({ ...formData, category: e })}
+        >
+          <SelectTrigger className="w-10/12 mx-auto">
+            <SelectValue className="w-10/12 mx-auto" placeholder="Category" />
+          </SelectTrigger>
+          <SelectContent className="">
+            <SelectGroup className="w-10/12 mx-auto">
+              <SelectItem name="category" id="gems" value="gems">
+                fuck
+              </SelectItem>
+
+              <SelectItem value="jewelry" name="category">
+                Jewelry
+              </SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <Input
+          value={formData.tags}
+          name="tags"
+          onChange={(e) => {
+            handleInputChange(e);
+          }}
+          className="w-10/12 mx-auto"
+          placeholder="Tags: separate with commas"
+        />
+        <Select onValueChange={(e) => setFormData({ ...formData, sale: e })}>
+          <SelectTrigger className="w-10/12 mx-auto">
+            <SelectValue
+              className="w-10/12 mx-auto"
+              placeholder="On Sale Status"
+            />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup className="w-10/12 mx-auto">
+              <SelectItem value="active" name="sale">
+                Active
+              </SelectItem>
+              <SelectItem value="inactive" name="sale">
+                Inactive
+              </SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+
+        <Select
+          onValueChange={(e) => setFormData({ ...formData, stockStat: e })}
+        >
+          <SelectTrigger className="w-10/12 mx-auto">
+            <SelectValue placeholder="In Stock Status" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem value="In Stock" name="stockStat">
+                In Stock
+              </SelectItem>
+              <SelectItem value="Out of Stock" name="stockStat">
+                Out of Stock
+              </SelectItem>
+              <SelectItem value="Pre-Order" name="stockStat">
+                Pre-Order
+              </SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <Input
+          onChange={(e) => {
+            handleInputChange(e);
+          }}
+          className="w-10/12 mx-auto"
+          placeholder="Enter Quantity For Out Of Stock..."
+          name="stock"
+          value={formData.stock}
+        />
+        <Textarea
+          onChange={(e) => {
+            handleInputChange(e);
+          }}
+          className="resize-none w-10/12 mx-auto h-1/2"
+          placeholder="Warranty Information"
+          name="warranty"
+          value={formData.warranty}
+        />
+        <Textarea
+          onChange={(e) => {
+            handleInputChange(e);
+          }}
+          className="resize-none w-10/12 mx-auto h-1/2"
+          placeholder="Additional Item Information..."
+          name="extra"
+          value={formData.extra}
+        />
+        <Input
+          onChange={(e) => {
+            handleInputChange(e);
+          }}
+          className="w-10/12 mx-auto"
+          placeholder="Weight"
+          name="weight"
+          value={formData.value}
+        />
+        <h1 className="w-10/12 text-red-500 text-xs">{missing}</h1>
+        <Button
+          onClick={submitItem}
+          // onClick={() => console.log(formData)}
+          className="w-10/12 mx-auto"
+        >
+          Add Item
+        </Button>
       </div>
     </div>
   );
 }
 
-export default addItem;
+export default AddItem;
