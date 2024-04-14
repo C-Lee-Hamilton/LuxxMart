25e1447df31330d87c62d77ae738099014206441
Cody Lee <notlimaheelydoc@gmail.com>
2024-04-14 18:30:55 -0500

    Form on Item Upload

--- src/Pages/SellerPages/AddItem.js
+++ src/Pages/SellerPages/AddItem.js
@@ -27,237 +27,297 @@ import { Button } from "../../Components/ui/button";
 import { Textarea } from "../../Components/ui/textarea";
 import ItemPhotoUpload from "../../Components/sellerComponents/itemPhotoUpload";
 
+const formSchema = z.object({
+  name: z.string().min(1, {
+    message: "Name Required",
+  }),
+
+  serial: z.string().min(1, {
+    message: "Serial Number Required",
+  }),
+
+  price: z.string().min(1, {
+    message: "Price Required",
+  }),
+
+  description: z.string().min(1, {
+    message: "Description Required",
+  }),
+  category: z.string().min(1, {
+    message: "Category Required",
+  }),
+  tags: z.string().min(1, {
+    message: "Tags Required",
+  }),
+
+  status: z.string().min(1, {
+    message: "Set Status of Item",
+  }),
+  warranty: z.string().min(0, {
+    message: "Not Required",
+  }),
+  weight: z.string().min(0, {
+    message: "Not Required",
+  }),
+  dimensions: z.string().min(0, {
+    message: "Not Required",
+  }),
+  extra: z.string().min(0, {
+    message: "Not Required",
+  }),
+});
+
 function AddItem() {
   const [fileUpload, setFileUpload] = useState([]);
-  const [missing, setMissing] = useState();
-  const { control } = useForm();
-  const formSchema = z.object({
-    username: z.string().min(2, {
-      message: "Name Required",
-    }),
-  });
-  const [formData, setFormData] = useState({
-    name: "",
-    serial: "",
-    price: "",
-    description: "",
-    category: "",
-    tags: "",
-    sale: "",
-    inStock: "",
-    stock: "",
-    warranty: "",
-    extra: "",
-    weight: "",
-  });
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const submitItem = () => {
-    const emptyFields = Object.entries(formData)
-      .filter(([key, value]) => value === "")
-      .map(([key]) => key)
-      .toString();
-    emptyFields.length > 0
-      ? setMissing("Missing Fields: All Fields Required")
-      : setMissing();
-  };
 
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      name: "",
+      serial: "",
+      price: "",
+      description: "",
+      category: "",
+      tags: "",
+      status: "",
+      warranty: "",
+      weight: "",
+      dimensions: "",
+      extra: "",
     },
   });
 
   function onSubmit(values) {
-    // Do something with the form values.
     console.log(values);
   }
 
   return (
-    <div className=" grid  sm:grid-cols-2 xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-3   border-black border-4 border-solid ">
-      <div className="w-full  xs:w-1/2 col-span-1 h-screen">
+    <div className=" grid  bg-black sm:bg-offwhite sm:grid-cols-2 xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-3    border-black border-4 border-solid ">
+      <div className="w-full  xs:w-1/2 col-span-1 h-full">
         <ItemPhotoUpload
           setFileUpload={setFileUpload}
           fileUpload={fileUpload}
         />
       </div>
 
-      {/* <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <Form {...form} className="col-span-2 w-full ">
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="grid  w-full  space-y-1 lg:space-y-4 lg:col-span-2  col-span-1"
+        >
+          <h1 className="mx-auto  mt-6 mb-3 text-xl sm:text-black text-gold  ">
+            Product Details
+          </h1>
           <FormField
-            control={control}
-            name="username"
+            control={form.control}
+            name="name"
             render={({ field }) => (
               <FormItem>
                 <FormControl>
-                  <Input placeholder="shadcn" {...field} />
+                  <Input
+                    className="w-10/12 mx-auto border-2 border-solid border-darkgold rounded-md"
+                    placeholder="Enter Item Name"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage className="w-10/12 mx-auto" />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="serial"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    className="w-10/12 mx-auto border-2 border-solid border-darkgold rounded-md"
+                    placeholder="Serial Number"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage className="w-10/12 mx-auto" />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="price"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    className="w-10/12 mx-auto border-2 border-solid border-darkgold rounded-md"
+                    placeholder="Price"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage className="w-10/12 mx-auto" />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Textarea
+                    className="w-10/12 mx-auto border-2 border-solid border-darkgold rounded-md"
+                    placeholder="Description..."
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage className="w-10/12 mx-auto" />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="category"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <div {...field}>
+                    <Select>
+                      <SelectTrigger className="w-10/12 mx-auto border-2 border-solid border-darkgold rounded-md">
+                        <SelectValue placeholder="Select Category" />
+                      </SelectTrigger>
+
+                      <SelectContent>
+                        <SelectGroup className="w-10/12 mx-auto">
+                          <SelectItem value="Gems">Gems</SelectItem>
+
+                          <SelectItem value="Jewelry">Jewelry</SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </div>
+                </FormControl>
+                <FormMessage className="w-10/12 mx-auto" />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="tags"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    className="w-10/12 mx-auto border-2 border-solid border-darkgold rounded-md"
+                    placeholder="Tags: separate by commas"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage className="w-10/12 mx-auto" />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="status"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <div {...field}>
+                    <Select>
+                      <SelectTrigger className="w-10/12 mx-auto border-2 border-solid border-darkgold rounded-md">
+                        <SelectValue placeholder="Select Item Status" />
+                      </SelectTrigger>
+
+                      <SelectContent>
+                        <SelectGroup className="w-10/12 mx-auto">
+                          <SelectItem value="Active">Active</SelectItem>
+
+                          <SelectItem value="Inactive">Inactive</SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </div>
+                </FormControl>
+                <FormMessage className="w-10/12 mx-auto" />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="warranty"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Textarea
+                    className="w-10/12 mx-auto border-2 border-solid border-darkgold rounded-md"
+                    placeholder="Warranty Information..."
+                    {...field}
+                  />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
-          <Button type="submit">Submit</Button>
+          <FormField
+            control={form.control}
+            name="weight"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    className="w-10/12 mx-auto border-2 border-solid border-darkgold rounded-md"
+                    placeholder="Weight"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="dimensions"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    className="w-10/12 mx-auto border-2 border-solid border-darkgold rounded-md"
+                    placeholder="Dimensions"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="extra"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    className="w-10/12 mx-auto border-2 border-solid border-darkgold rounded-md"
+                    placeholder="Addtional Information"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="w-full flex">
+            <Button
+              className="w-10/12 mx-auto mt-2 mb-5 border-2 border-solid border-darkgold rounded-md"
+              type="submit"
+            >
+              Submit
+            </Button>
+          </div>
         </form>
-      </Form> */}
-
-      <div className="grid    mt-5 grid-cols-1 col-span-1 lg:col-span-2 lg:grid-cols-2 ">
-        <Input
-          onChange={(e) => {
-            handleInputChange(e);
-          }}
-          className="w-10/12 mx-auto"
-          placeholder="Enter Item Name..."
-          type="text"
-          name="name"
-          value={formData.name}
-        />
-
-        <Input
-          onChange={(e) => {
-            handleInputChange(e);
-          }}
-          className="w-10/12 mx-auto"
-          placeholder="Serial Number"
-          name="serial"
-          value={formData.serial}
-        />
-        <Input
-          onChange={(e) => {
-            handleInputChange(e);
-          }}
-          className="w-10/12 mx-auto"
-          placeholder="Price"
-          name="price"
-          value={formData.price}
-        />
-        <Textarea
-          onChange={(e) => {
-            handleInputChange(e);
-          }}
-          className="resize-none w-10/12 mx-auto h-1/2"
-          placeholder="Item Description..."
-          name="description"
-          value={formData.description}
-        />
-        <Select
-          onValueChange={(e) => setFormData({ ...formData, category: e })}
-        >
-          <SelectTrigger className="w-10/12 mx-auto">
-            <SelectValue className="w-10/12 mx-auto" placeholder="Category" />
-          </SelectTrigger>
-          <SelectContent className="">
-            <SelectGroup className="w-10/12 mx-auto">
-              <SelectItem name="category" id="gems" value="gems">
-                fuck
-              </SelectItem>
-
-              <SelectItem value="jewelry" name="category">
-                Jewelry
-              </SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-        <Input
-          value={formData.tags}
-          name="tags"
-          onChange={(e) => {
-            handleInputChange(e);
-          }}
-          className="w-10/12 mx-auto"
-          placeholder="Tags: separate with commas"
-        />
-        <Select onValueChange={(e) => setFormData({ ...formData, sale: e })}>
-          <SelectTrigger className="w-10/12 mx-auto">
-            <SelectValue
-              className="w-10/12 mx-auto"
-              placeholder="On Sale Status"
-            />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup className="w-10/12 mx-auto">
-              <SelectItem value="active" name="sale">
-                Active
-              </SelectItem>
-              <SelectItem value="inactive" name="sale">
-                Inactive
-              </SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-
-        <Select
-          onValueChange={(e) => setFormData({ ...formData, stockStat: e })}
-        >
-          <SelectTrigger className="w-10/12 mx-auto">
-            <SelectValue placeholder="In Stock Status" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value="In Stock" name="stockStat">
-                In Stock
-              </SelectItem>
-              <SelectItem value="Out of Stock" name="stockStat">
-                Out of Stock
-              </SelectItem>
-              <SelectItem value="Pre-Order" name="stockStat">
-                Pre-Order
-              </SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-        <Input
-          onChange={(e) => {
-            handleInputChange(e);
-          }}
-          className="w-10/12 mx-auto"
-          placeholder="Enter Quantity For Out Of Stock..."
-          name="stock"
-          value={formData.stock}
-        />
-        <Textarea
-          onChange={(e) => {
-            handleInputChange(e);
-          }}
-          className="resize-none w-10/12 mx-auto h-1/2"
-          placeholder="Warranty Information"
-          name="warranty"
-          value={formData.warranty}
-        />
-        <Textarea
-          onChange={(e) => {
-            handleInputChange(e);
-          }}
-          className="resize-none w-10/12 mx-auto h-1/2"
-          placeholder="Additional Item Information..."
-          name="extra"
-          value={formData.extra}
-        />
-        <Input
-          onChange={(e) => {
-            handleInputChange(e);
-          }}
-          className="w-10/12 mx-auto"
-          placeholder="Weight"
-          name="weight"
-          value={formData.value}
-        />
-        <h1 className="w-10/12 text-red-500 text-xs">{missing}</h1>
-        <Button
-          onClick={submitItem}
-          // onClick={() => console.log(formData)}
-          className="w-10/12 mx-auto"
-        >
-          Add Item
-        </Button>
-      </div>
+      </Form>
     </div>
   );
 }
 
 export default AddItem;
+
+// /* <div className="grid    mt-5 grid-cols-1 col-span-1 lg:col-span-2 lg:grid-cols-2 ">
